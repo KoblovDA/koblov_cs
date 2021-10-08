94f9ac5cd3562c72f2f8e472d56310a9d3b11f5a
Koblov Daniil <koblov.da@phystech.edu>
2021-10-08 11:59:12 +0300

    Counter of success

--- lab5/Task1.py
+++ lab5/Task1.py
@@ -1,10 +1,10 @@
 import pygame
 from pygame.draw import *
 from random import randint
-
+import pygame.freetype
 pygame.init()
-
-FPS = 1.7
+GAME_FONT = pygame.freetype.Font("arial.ttf", 60)
+FPS = 2
 screen = pygame.display.set_mode((900, 900))
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -14,7 +14,7 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
-font = pygame.font.SysFont("arial", 30)
+font = pygame.font.SysFont('Arial', 10, False, False)
 x, y, r = 0, 0, 0
 
 
@@ -34,8 +34,10 @@ pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 points = 0
+success = -1
 while not finished:
     clock.tick(FPS)
+    screen.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
@@ -45,15 +47,17 @@ while not finished:
             if (event.x - x) ** 2 + (event.y - y) ** 2 < r ** 2:
                 print('Good hit)')
                 points += 1
-                text = pygame.font.Font.render(font, "Bad hit... Points: " + str(points), 1, YELLOW, BLUE)
-                pygame.Surface.blit(screen, text, (0, 0))
+                success = 1
             else:
                 print('Bad hit(')
-                text = pygame.font.Font.render(font, "Bad hit... Points: " + str(points), 1, YELLOW, BLUE)
-                pygame.Surface.blit(screen, text, (0, 0))
-    screen.fill(BLACK)
+                success = 0
+    if success == 1:
+        text_surface, rect = GAME_FONT.render("Good hit) Points: " + str(points), YELLOW)
+        screen.blit(text_surface, (70, 120))
+    elif success == -0:
+        text_surface, rect = GAME_FONT.render("Bad hit... Points: " + str(points), YELLOW)
+        screen.blit(text_surface, (70, 120))
     new_ball()
     draw_ball()
     pygame.display.update()
-
 pygame.quit()
