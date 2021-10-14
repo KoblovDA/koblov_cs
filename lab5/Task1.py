ddb657fa2923e0f402784bde84e564971b3953db
Koblov Daniil <koblov.da@phystech.edu>
2021-10-14 13:32:37 +0300

    Added a motion of balls

--- lab5/Task1.py
+++ lab5/Task1.py
@@ -1,9 +1,9 @@
 from pygame.draw import *
 from random import randint
 import pygame.freetype
+
 pygame.init()
 GAME_FONT = pygame.freetype.Font("arial.ttf", 60)
-FPS = 2
 screen = pygame.display.set_mode((900, 900))
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
@@ -13,16 +13,17 @@ MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 FONT_COLOR = (160, 70, 70)
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]  # цвета шариков
 font = pygame.font.SysFont('Arial', 10, False, False)
 x_0, y_0, r_0 = 0, 0, 0
+MAX, MIN, FINE = 20, 10, -2  # количество очков за попадание в маленький, большой шарик, штраф за промах
 
 
 def new_ball():
     global x_0, y_0, r_0
     x_0 = randint(200, 700)
     y_0 = randint(200, 500)
-    r_0 = randint(30, 50)
+    r_0 = randint(30, 60)
 
 
 def draw_ball(scr):
@@ -35,31 +36,48 @@ clock = pygame.time.Clock()
 finished = False
 points = 0
 success = -1
-time = 0
+success_in_time = -1
+time_of_game = 0
+time_of_ball = 0
+u_x = 0
+u_y = 0
 while not finished:
+    FPS = randint(1, 2)
     clock.tick(FPS)
-    time += FPS
+    time_of_game += FPS
     screen.fill(BLACK)
     for event in pygame.event.get():
-        if (event.type == pygame.QUIT) or (time >= 100):
+        if (event.type == pygame.QUIT) or (time_of_game >= 100):
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             event.x = event.pos[0]
             event.y = event.pos[1]
             if (event.x - x_0) ** 2 + (event.y - y_0) ** 2 < r_0 ** 2:
-                print('Good hit)')
-                points += 1
+                if r_0 < 45:
+                    points += MAX
+                else:
+                    points += MIN
                 success = 1
+                success_in_time == 1
             else:
-                print('Bad hit(')
                 success = 0
+                points += -FINE
     if success == 1:
         text_surface, rect = GAME_FONT.render("Good hit) Points: " + str(points), FONT_COLOR)
         screen.blit(text_surface, (70, 120))
     elif success == -0:
         text_surface, rect = GAME_FONT.render("Bad hit... Points: " + str(points), FONT_COLOR)
         screen.blit(text_surface, (70, 120))
-    new_ball()
+    time_of_ball += 1
+    if (time_of_ball >= 10) or (u_x == 0) or (success_in_time == 1):
+        new_ball()
+        time_of_ball = 0
+        u_x = randint(50, 120)
+        u_y = randint(50, 120)
+    else:
+        x_0 += u_x
+        y_0 += u_y
     draw_ball(screen)
+    success_in_time = -1
     pygame.display.update()
 pygame.quit()
