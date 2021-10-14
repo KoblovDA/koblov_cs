0033e5962f8833f82a7bee985b79afb473307e81
Koblov Daniil <koblov.da@phystech.edu>
2021-10-14 13:06:04 +0300

    Refaroring of functions

--- lab5/Task1.py
+++ lab5/Task1.py
@@ -13,21 +13,22 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
+FONT_COLOR = (120, 70, 70)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 font = pygame.font.SysFont('Arial', 10, False, False)
-x, y, r = 0, 0, 0
+x_0, y_0, r_0 = 0, 0, 0
 
 
 def new_ball():
-    global x, y, r
-    x = randint(100, 700)
-    y = randint(100, 500)
-    r = randint(30, 50)
+    global x_0, y_0, r_0
+    x_0 = randint(100, 700)
+    y_0 = randint(100, 500)
+    r_0 = randint(30, 50)
 
 
-def draw_ball():
+def draw_ball(scr):
     color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
+    circle(scr, color, (x_0, y_0), r_0)
 
 
 pygame.display.update()
@@ -44,7 +45,7 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             event.x = event.pos[0]
             event.y = event.pos[1]
-            if (event.x - x) ** 2 + (event.y - y) ** 2 < r ** 2:
+            if (event.x - x_0) ** 2 + (event.y - y_0) ** 2 < r_0 ** 2:
                 print('Good hit)')
                 points += 1
                 success = 1
@@ -52,12 +53,12 @@ while not finished:
                 print('Bad hit(')
                 success = 0
     if success == 1:
-        text_surface, rect = GAME_FONT.render("Good hit) Points: " + str(points), YELLOW)
+        text_surface, rect = GAME_FONT.render("Good hit) Points: " + str(points), FONT_COLOR)
         screen.blit(text_surface, (70, 120))
     elif success == -0:
-        text_surface, rect = GAME_FONT.render("Bad hit... Points: " + str(points), YELLOW)
+        text_surface, rect = GAME_FONT.render("Bad hit... Points: " + str(points), FONT_COLOR)
         screen.blit(text_surface, (70, 120))
     new_ball()
-    draw_ball()
+    draw_ball(screen)
     pygame.display.update()
 pygame.quit()
