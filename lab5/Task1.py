6a8b5d7226f72baa1f2a3953b8ac8996b62e4730
Koblov Daniil <koblov.da@phystech.edu>
2021-10-15 01:34:59 +0300

    Пояснения к функциям

--- lab5/Task1.py
+++ lab5/Task1.py
@@ -5,20 +5,23 @@ import pygame.freetype
 pygame.init()
 GAME_FONT = pygame.freetype.Font("arial.ttf", 40)
 screen = pygame.display.set_mode((800, 600))
+RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN = (255, 0, 0), (0, 0, 255), (255, 255, 0), (0, 255, 0), (255, 0, 255), (
+    0, 255, 255)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-MAGENTA = (255, 0, 255)
-CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
-FONT_COLOR = (160, 70, 70)
+FONT_COLOR = (200, 70, 130)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]  # цвета шариков
-MAX, MIN, FINE = 20, 10, -2  # количество очков за попадание в маленький, большой шарик, штраф за промах
+MAX, MIN, FINE = 20, 10, -5  # количество очков за попадание в маленький, большой шарик, штраф за промах
+NUMBER_OF_BALLS = 10
 
 
 def new_ball():
+    """
+    Функция создаёт новый шарик с рандомными параметрами в указанных диапазонах -
+    x_0, y_0 - координаты, r_0 - радиус шарика, u_x и u_y - скорости шарика по указанным осям.
+    color - цвет шарика из листа цветов, указанных ранее.
+    Выдаёт лист данных, который характеризует его, который далее в программе используется в листе со всеми шариками
+    """
     x_0 = randint(200, 700)
     y_0 = randint(200, 500)
     r_0 = randint(30, 60)
@@ -29,10 +32,18 @@ def new_ball():
 
 
 def draw_ball(scr, parameters):
+    """
+    Принимает экран и параметры шарика (записанные в форме, как в прошлой функции), то есть использует
+    координаты, цвет и радиус шарика
+    """
     circle(scr, parameters[6], (parameters[0], parameters[1]), parameters[2])
 
 
 def change_of_color(clr, rand):
+    """
+    Функция, добавляющая отличительную черту геймплея - малые колебания цвета шариков со временем.
+    Принимает исходный цвет и число, которое характеризует размах колебаний
+    """
     new_color = [clr[0], clr[1], clr[2]]
     for i in range(0, 3):
         colour = new_color[i]
@@ -46,7 +57,7 @@ def change_of_color(clr, rand):
 
 
 balls = []
-for i in range(0, 10):
+for i in range(0, NUMBER_OF_BALLS):
     balls.append([0, 0, 0, 0, 0, 0, 0])
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -62,12 +73,12 @@ while not finished:
     time_of_game += FPS
     screen.fill(WHITE)
     for event in pygame.event.get():
-        if (event.type == pygame.QUIT) or (time_of_game >= 10000):
+        if (event.type == pygame.QUIT) or (time_of_game >= 100000):
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             event.x = event.pos[0]
             event.y = event.pos[1]
-            for i in range(0, 10):
+            for i in range(0, NUMBER_OF_BALLS):
                 x_ball, y_ball, r_ball = balls[i][0], balls[i][1], balls[i][2]
                 if (event.x - x_ball) ** 2 + (event.y - y_ball) ** 2 < r_ball ** 2:
                     num_of_success = i
@@ -98,7 +109,7 @@ while not finished:
         else:
             balls[i][0] += balls[i][3]
             balls[i][1] += balls[i][4]
-            balls[i][6] = change_of_color(balls[i][6], 10)
+            balls[i][6] = change_of_color(balls[i][6], NUMBER_OF_BALLS)
         if balls[i][0] + balls[i][2] > 799:
             balls[i][3] = -balls[i][3]
             balls[i][0] -= 10
