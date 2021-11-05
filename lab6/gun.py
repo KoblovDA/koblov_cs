af6d27754875e3015b8a535e5c48135ab1c0ce5b
Koblov Daniil <koblov.da@phystech.edu>
2021-11-05 08:42:22 +0300

    Комментарии к функциям

--- lab6/gun.py
+++ lab6/gun.py
@@ -23,6 +23,10 @@ g = 2
 
 class Shot:
     def __init__(self):
+        """
+        Конструирует выстрел шариком или лазером.
+        x и y - центр шарика или начальная точка выстрела лазера - оба совпадают с координатами пушки
+        """
         global gun, screen
         self.x = gun.x
         self.y = gun.y
@@ -30,12 +34,9 @@ class Shot:
 
 
 class Ball(Shot):
-    def __init__(self, screen: pygame.Surface, gun):
+    def __init__(self):
         """ Конструктор класса ball
-
-        Args:
-        x - начальное положение мяча по горизонтали
-        y - начальное положение мяча по вертикали
+        r - радиус шара, vx и vy - скорости по осям, color - цвет, live - параметр жизни
         """
 
         super(Ball, self).__init__()
@@ -50,7 +51,8 @@ class Ball(Shot):
         """Переместить мяч по прошествии единицы времени.
 
         Метод описывает перемещение мяча за один кадр перерисовки. То есть, обновляет значения
-        self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации, действующей на мяч,
+        self.x и self.y с учетом скоростей self.vx и self.vy, силы гравитации и силы сопротивления воздуха, действующих
+        на мяч,
         и стен по краям окна (размер окна 800х600).
         """
         self.vy = (self.vy - g) * 0.99
@@ -80,6 +82,7 @@ class Ball(Shot):
 
         Args:
             obj: Обьект, с которым проверяется столкновение.
+            num: 0 для столкновения с круглой целью и 1 для столкновения с квадратом
         Returns:
             Возвращает True в случае столкновения мяча и цели. В противном случае возвращает False.
         """
@@ -90,22 +93,37 @@ class Ball(Shot):
 
 
 class Laser(Shot):
-    def __init__(self, screen, gun):
+    def __init__(self):
+        """
+        Инициализация лазера. width - параметр ширины линии, которая должна рисоваться,
+        angle - угол, под которым стреляет лазер (зависит от положения мыши в данный момент),
+        live - параметр жизни (лазер должен исчезать через некоторое время после применения)
+        """
         super(Laser, self).__init__()
         self.width = 5
         self.angle = 1
         self.live = 0
 
     def draw(self):
+        """
+        Прорисовка лазера. Рисует его как линию с заданными наклоном, определяемым мышью, уходящей до конца экрана."
+        """
         pygame.draw.line(self.screen, RED, (self.x + 60 * math.cos(self.angle), self.y - 60 * math.sin(self.angle)),
                          (self.x + 1000 * math.cos(self.angle), self.y - math.sin(self.angle) * 1000), self.width)
 
     def move(self, event, gun):
+        """
+        Движение лазера, учитывающее движение пушки и её поворот
+        """
         self.angle = -math.atan2((event.pos[1] - self.y), (event.pos[0] - self.x))
         self.x = gun.x
         self.y = gun.y
 
     def hit_test(self, obj):
+        """
+        Проверка на попадание лазера в КРУГЛУЮ цель с помощью формулы расстояния от точки до прямой.
+        Квадратные цели лазер не задевает.
+        """
         normal = [math.tan(self.angle), 1]
         return abs(((normal[0] * (obj.x - self.x) + normal[1] * (obj.y - self.y)) / (
                 normal[0] ** 2 + normal[1] ** 2) ** 0.5)) <= obj.r
@@ -113,6 +131,10 @@ class Laser(Shot):
 
 class Gun:
     def __init__(self, screen):
+        """
+        Инициализация пушки. f2_on и f2_power - параметры, отвечающие за то, пытается ли выстрелить в данный момент
+        пушка шариком. an - угол поворота пушки, x и y - координаты левой нижней точки пушки.
+        """
         self.screen = screen
         self.f2_power = 10
         self.f2_on = 0
@@ -122,6 +144,9 @@ class Gun:
         self.y = 450
 
     def move(self, key):
+        """
+        Реализовано стандартное движение в соответствии с нажатыми кнопками - WASD
+        """
         if key[pygame.K_d]:
             self.x += 5
         if key[pygame.K_a]:
@@ -142,7 +167,7 @@ class Gun:
         """
         global balls, bullet
         bullet += 1
-        new_ball = Ball(self.screen, Gun)
+        new_ball = Ball()
         new_ball.r += 5
         self.an = -math.atan2((event.pos[1] - self.y), (event.pos[0] - self.x))
         new_ball.vx = self.f2_power * math.cos(self.an)
@@ -151,7 +176,7 @@ class Gun:
         self.f2_on = 0
         self.f2_power = 10
 
-    def targetting(self, event):
+    def targeting(self, event):
         """Прицеливание. Зависит от положения мыши."""
         if event:
             self.an = -math.atan2((event.pos[1] - self.y), (event.pos[0] - self.x))
@@ -161,6 +186,9 @@ class Gun:
             self.color = GREY
 
     def draw(self):
+        """
+        Прорисовка пушки как удлинняющегося прямоугольника в направлении, указанном мышкой. Цвет пушки изменяется
+        """
         delta = (50 + self.f2_power)
         pygame.draw.polygon(self.screen, (2 * self.f2_power, 2 * self.f2_power, self.f2_power),
                             ((self.x, self.y), (self.x + delta * math.cos(self.an), self.y - delta * math.sin(self.an)),
@@ -170,6 +198,9 @@ class Gun:
                               self.y - 10 * math.cos(self.an))))
 
     def power_up(self):
+        """
+        Доп функция, связанная с удлиннением пушки
+        """
         if self.f2_on:
             if self.f2_power < 100:
                 self.f2_power += 1
@@ -180,6 +211,10 @@ class Gun:
 
 class Enemy:
     def __init__(self):
+        """
+        Инициализируется класс врагов, появляющихся в случайном месте со случайными скоростями и параметром жизни
+        live, необходимым для удаления его при попадании в него.
+        """
         self.x = rnd(600, 780)
         self.y = rnd(300, 550)
         self.vx = rnd(-10, 10)
@@ -187,6 +222,9 @@ class Enemy:
         self.live = 1
 
     def move(self):
+        """
+        Движение врага без учёта гравитации с учётом отражения от стенок
+        """
         self.x += self.vx
         self.y -= self.vy
         if self.x > 800 - self.r:
@@ -205,23 +243,31 @@ class Enemy:
 
 class Target(Enemy):
     def __init__(self):
-        """ Инициализация новой цели. """
+        """ Инициализация круглого врага со случайными радиусом и цветом """
         super(Target, self).__init__()
         self.r = rnd(10, 50)
         self.color = GAME_COLORS[rnd(0, 5)]
 
     def draw(self):
+        """
+        Прорисовка круглого врага
+        """
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.r)
 
 
 class Square(Enemy):
     def __init__(self):
+        """Аналогично с круглым"""
         super(Square, self).__init__()
         self.color = BLACK
         self.live = 1
         self.r = rnd(5, 15)
 
     def jump(self):
+        """
+        Особенность движения квадратного врага - он может прыгнуть на фиксированное расстояние вниз-вверх или
+        вправо-влево (или одновременно в две стороны)
+        """
         if 700 - self.r > self.x > 100 + self.r and 100 + self.r < self.y < 500 - self.r:
             if rnd(0, 100) < 25:
                 rnd_x = rnd(-1, 1)
@@ -235,6 +281,9 @@ class Square(Enemy):
 
 class Bomb:
     def __init__(self):
+        """
+        Инициализация бомб, падающих сверху из случайного места с разбросом скоростей
+        """
         self.x = rnd(50, 750)
         self.y = -10
         self.vy = rnd(6, 15)
@@ -243,12 +292,15 @@ class Bomb:
         self.live = 1
 
     def move(self):
+        """Движение бомбы"""
         self.y += self.vy
 
     def draw(self):
+        """Прорисовка бомбы"""
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.r, 2)
 
     def hit_test(self, obj):
+        """Проверяет, столкнулась ли бомба с объектом (пушкой)"""
         return (obj.x - self.x) ** 2 + (obj.y - self.y) ** 2 <= self.r ** 2
 
 
@@ -263,7 +315,7 @@ bombs = []
 
 clock = pygame.time.Clock()
 gun = Gun(screen)
-laser = Laser(screen, gun)
+laser = Laser()
 targets = [Target() for _ in range(5)]
 squares = [Square() for _ in range(2)]
 bombs = [Bomb() for _ in range(5)]
@@ -288,7 +340,6 @@ while not finished:
         obj.draw()
         if obj.hit_test(gun):
             text_surface, rect = GAME_FONT.render("GAME OVER" + str(score), BLACK)
-            clock.tick(3 * FPS)
             finished = True
 
     pygame.display.update()
@@ -310,7 +361,7 @@ while not finished:
                 gun.fire2_end(event)
         elif event.type == pygame.MOUSEMOTION:
             laser.move(event, gun)
-            gun.targetting(event)
+            gun.targeting(event)
     for b in balls:
         b.move()
         for obj in targets:
